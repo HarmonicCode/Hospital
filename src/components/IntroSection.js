52b154a5589082d4c2f86e8ef76ded1bf424d654
HarmonicCode <sharmasimran91262@gmail.com>
2024-09-08 20:15:00 +0530

    first

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -1,27 +1,30 @@
 import React from "react";
-import { Button } from './Button';
-import "./IntroSection.css";
+import { Button } from './Button';  // Correct path for Button.js in the same folder
+import "./IntroSection.css";  // Correct path for CSS file in the same folder
 
-
-
-
-function IntroSection(){
-    return(
+function IntroSection() {
+    return (
         <div className="Intro-container">
-            <img src={process.env.PUBLIC_URL + "/images/slider-img.jpg"} alt="slider" />
+            <img
+                src={process.env.PUBLIC_URL + "/images/slider-img.jpg"}
+                alt="slider"
+            />
             <h1>Medscraft Healthcare</h1>
-            <p>At Medscraft Healthcare, we envision a healthier, more equitable society where everyone has access to essential healthcare. We strive to set new standards for affordability and excellence, inspiring others to join us in our mission of making healthcare accessible to all.</p>
-            <div className ="Intro-btns">
-                <Button className ="btns" buttonStyle="btn--outline" buttonSize="btn--large">
+            <p>
+                At Medscraft Healthcare, we envision a healthier, more equitable society
+                where everyone has access to essential healthcare. We strive to set new
+                standards for affordability and excellence, inspiring others to join us
+                in our mission of making healthcare accessible to all.
+            </p>
+            <div className="Intro-btns">
+                <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
                     Book Appointment
                 </Button>
-                <Button className ="btns" buttonStyle="btn--primary" buttonSize="btn--large">
+                <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">
                     Book Appointment
                 </Button>
             </div>
         </div>
-        
-
     );
 }
 
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,5 +1,5 @@
 import "../../App.css";
-import IntroSection from "./IntroSection";
+import IntroSection from '../IntroSection';  // Correct path from pages to components
 
 
 function Home(){
